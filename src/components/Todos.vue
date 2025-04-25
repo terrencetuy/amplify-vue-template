62ddfd55f3527e742960cd67a688cd44020ed906
Terrence Tuy <terrence.tuy@gmail.com>
2025-04-24 21:57:37 -0400

    added authenticator

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -31,6 +31,10 @@ function createTodo() {
   listTodos();
 });
 
+function deleteTodo(id: string) {
+  client.models.Todo.delete({ id })
+}
+
 </script>
 
 <template>
@@ -40,7 +44,9 @@ function createTodo() {
     <ul>
       <li 
         v-for="todo in todos" 
-        :key="todo.id">
+        :key="todo.id"
+        @click="deleteTodo(todo.id)"
+      >
         {{ todo.content }}
       </li>
     </ul>
